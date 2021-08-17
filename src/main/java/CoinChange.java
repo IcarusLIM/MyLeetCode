efa9458c1f88dbb6313d6e6097d8a4f39fe043a7
guangwei <xyty007@gmail.com>
2021-08-17 15:03:11 +0800

    add CoinChange

---
+++ src/main/java/CoinChange.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import java.util.Arrays;
+
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+        dp[0] = -1;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = -1;
+            for (int coin : coins) {
+                int remain = i - coin;
+                if (remain <= 0) {
+                    if (remain == 0) {
+                        dp[i] = 1;
+                    }
+                    break;
+                }
+                if (dp[remain] > 0 && (dp[i] < 0 || dp[i] > dp[remain] + 1)) {
+                    dp[i] = dp[remain] + 1;
+                }
+            }
+        }
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        CoinChange cc = new CoinChange();
+        System.out.println(cc.coinChange(new int[]{1, 2, 5}, 11));
+    }
+}
